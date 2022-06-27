a83f7d87fa5c56064ba2c60989b0a39ef1a347d1
haiou1314 <1871943511@qq.com>
2022-06-27 19:48:50 +0800

    引入style-loader和css-loader

--- src/index.js
+++ src/index.js
@@ -3,5 +3,8 @@ import './banner.js'
 //引入tabs.js
 import './tabs.js'
 //引入jquery
-import $ from 'jquery';
-$('#swiper').css('background-color', 'red')
+import $ from 'jquery'
+//引入index.css
+import './styles/index.css'
+
+$('#swiper').css('background-color', '')
